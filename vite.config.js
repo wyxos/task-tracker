ae258b07e7aa12abd38790e622188c8398fb71fd
Joey J <joey@orckid.com>
2022-10-16 18:28:17 +0000

    =feat: vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,44 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
+import mkcert from 'vite-plugin-mkcert'
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
+  server: {
+    https: true,
+    hmr: {
+      host: '127.0.0.1'
+    },
+    host: '127.0.0.1'
+  },
+  plugins: [
+    mkcert(),
+    vue({
+      template: {
+        transformAssetUrls: {
+          // The Vue plugin will re-write asset URLs, when referenced
+          // in Single File Components, to point to the Laravel web
+          // server. Setting this to `null` allows the Laravel plugin
+          // to instead re-write asset URLs to point to the Vite
+          // server instead.
+          base: null,
+
+          // The Vue plugin will parse absolute URLs and treat them
+          // as absolute paths to files on disk. Setting this to
+          // `false` will leave absolute URLs un-touched so they can
+          // reference assets in the public directly as expected.
+          includeAbsolute: false,
+        },
+      },
+    }),
+    laravel({
+      input: [
+        'resources/css/app.css',
+        'resources/js/app.js',
+        'resources/app/js/main.js',
+        'resources/admin/js/main.js'
+      ],
+      refresh: true,
+    }),
+  ],
 });
